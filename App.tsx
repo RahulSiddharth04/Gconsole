4dc2d9922908790a5b98e27dd9c19f5c7676b3a7
RahulSiddharth04 <125539421+RahulSiddharth04@users.noreply.github.com>
2024-10-27 18:41:30 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import { Provider } from 'react-redux';
+import store from './src/store';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { Camera } from 'react-native-vision-camera';
+import Navigation from './src/navigation';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Scanner from './src/screens/main/Scanner';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App(): React.JSX.Element {
+ 
+  return (
+  //  <Scanner></Scanner>
+    <Provider store={store}> 
+      <Navigation/>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
